58f15472be5794521f5e3b0154206856b5e2ebbd
Loes Immens <loes.immens@alliander.com>
2024-10-02 16:02:19 +0200

    FDP-2638: fix integration test

--- application/src/integrationTest/kotlin/org/gxf/crestdeviceserviceweb/CrestDeviceServiceWebApplicationTests.kt
+++ application/src/integrationTest/kotlin/org/gxf/crestdeviceserviceweb/CrestDeviceServiceWebApplicationTests.kt
@@ -5,10 +5,8 @@ package org.gxf.crestdeviceserviceweb
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.kafka.test.context.EmbeddedKafka
 
 @SpringBootTest
-@EmbeddedKafka(topics = ["avroTopic"])
 class CrestDeviceServiceWebApplicationTests {
 
     @Test fun contextLoads() {}
