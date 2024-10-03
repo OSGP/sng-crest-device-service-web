a03bef488f9a95e71ae5e9e976ea6b90406e25e1
Loes Immens <loes.immens@alliander.com>
2024-10-03 16:56:38 +0200

    FDP-2638: firmware upload, REST call and mapping draft
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdeviceserviceweb/web/FirmwareFileDTO.kt
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: Contributors to the GXF project
-//
-// SPDX-License-Identifier: Apache-2.0
-package org.gxf.crestdeviceserviceweb.web
-
-data class FirmwareFileDTO(val packets: List<FirmwarePacket>) {
-    data class FirmwarePacket(val packetNumber: Int, val packet: String)
-}
--- application/src/main/kotlin/org/gxf/crestdeviceserviceweb/web/FirmwareService.kt
+++ application/src/main/kotlin/org/gxf/crestdeviceserviceweb/web/FirmwareService.kt
@@ -1,32 +1,34 @@
-// SPDX-FileCopyrightText: Contributors to the GXF project
+// SPDX-FileCopyrightText: Copyright Contributors to the GXF project
 //
 // SPDX-License-Identifier: Apache-2.0
 package org.gxf.crestdeviceserviceweb.web
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import io.github.oshai.kotlinlogging.KotlinLogging
+import org.gxf.crestdeviceserviceweb.http.HttpClient
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
 @Service
-class FirmwareService {
+class FirmwareService(val httpClient: HttpClient) {
     private val logger = KotlinLogging.logger {}
-    private val mapper = jacksonObjectMapper()
 
     fun processFirmwareFile(file: MultipartFile): Int {
         logger.info { "Processing file: ${file.originalFilename}" }
         val firmwareFileDTO = mapFirmwareFileToDTO(file)
         logger.info { "Processing ${firmwareFileDTO.packets.size} packets from firmware file" }
 
+        httpClient.postFirmware(firmwareFileDTO)
+
         return firmwareFileDTO.packets.size
     }
 
-    fun mapFirmwareFileToDTO(file: MultipartFile): FirmwareFileDTO {
+    fun mapFirmwareFileToDTO(file: MultipartFile): FirmwareDTO {
         val fileContent = String(file.inputStream.readBytes())
 
         logger.debug { "Contents of firmware file:\n${fileContent}" }
 
-        return mapper.readValue<FirmwareFileDTO>(fileContent)
+        val packets = fileContent.lines()
+
+        return FirmwareDTO(file.originalFilename!!, packets)
     }
 }
